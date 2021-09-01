88e19f34d814f572cee6b35fb2c8683749cdbc6d
ywangela <luckygirl666123@163.com>
2021-09-01 13:56:10 +0800

    主体区域完善和商品填充

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # imitative-TaoBao
 使用html，css和javascript，仿写了淘宝PC商城的首页
+
+### 项目介绍
+>仿写页面的过程中，用到的一些chrome插件：**Felpher**、**Visbug**
+
+- css实现的功能：下拉菜单 
+- js实现的功能：实现了轮播图、选项卡切换、搜索词联想（模拟数据），返回顶部，左侧标题联动内容等功能
+
--- js/main2-notice.js
+++ js/main2-notice.js
@@ -1,15 +1,16 @@
- let lis = document.querySelectorAll('.notice ul>li');
+ let liAs = document.querySelectorAll('.notice ul>li>a');
+ 
  // console.log(lis);
  let notConts = document.querySelectorAll('.notice-content>div');
  
  
- for (let i=0;i<lis.length;i++) { 
- 	lis[i].onmouseover = function(){
+ for (let i=0;i<liAs.length;i++) { 
+ 	liAs[i].onmouseover = function(){
         if(i === 0){
-			lis[0].children[0].style.borderBottom = '2px solid #ff4400';
+			liAs[0].style.borderBottom = '2px solid #ff4400';
 		}
 		if( i !== 0 ){
-			lis[0].children[0].style.borderBottom = 'none';
+			liAs[0].style.borderBottom = 'none';
 		}
 		for (let j=0;j<notConts.length;j++) {
 			notConts[j].style.display = 'none';
